777982172e14dd6e6bc731e37b7792153489417e
Juan Sebastian Reyes Carreño <jsrc1990@hotmail.com>
2021-09-09 13:01:30 -0500

    Agregada la clase EssentialsDevice

---
+++ Xamarin.Forms.Implementations/EssentialsDevice.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using TransversalLibrary.Standard;
+using Xamarin.Essentials;
+
+namespace Xamarin.Forms.Implementations
+{
+    /// <summary>
+    /// Define la implementación de la librería Essentials para usar las capacidades de los dispositivos
+    /// </summary>
+    public static class EssentialsDevice
+    {
+        /// <summary>
+        /// Captura una foto
+        /// </summary>
+        /// <returns>El Stream de la foto</returns>
+        public static async Task<Response<Stream>> CapturePhotoAsync()
+        {
+            Response<Stream> response = new Response<Stream>();
+            try
+            {
+                FileResult photo = await MediaPicker.CapturePhotoAsync();
+                response.Data = await photo.OpenReadAsync();
+            }
+            catch (Exception ex)
+            {
+                response.Exception = ex;
+            }
+            return response;
+        }
+
+        /// <summary>
+        /// Escoge la foto desde la galería
+        /// </summary>
+        /// <returns>El Stream de la foto</returns>
+        public static async Task<Response<Stream>> PickPhotoAsync()
+        {
+            Response<Stream> response = new Response<Stream>();
+            try
+            {
+                FileResult photo = await MediaPicker.PickPhotoAsync();
+                response.Data = await photo.OpenReadAsync();
+            }
+            catch (Exception ex)
+            {
+                response.Exception = ex;
+            }
+            return response;
+        }
+    }
+}
